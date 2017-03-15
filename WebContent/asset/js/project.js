c66da04046d46e9419af20e2cfdeb37fa2797dc5
Cloudzii <jwheo0920@gmail.com>
2017-03-15 18:28:51 +0900

    170315

--- WebContent/asset/js/project.js
+++ WebContent/asset/js/project.js
@@ -13,28 +13,63 @@ $(document).ready(function(){
     //   return false;
     // })
 
+    // $(document).ready(function(){
+    //     $('.btn-next-arr').click(function(){
+    //           $('html,body').animate({'scrollTop' : $(this.hash).offset().top},500);
+    //           return false;
+    //     });
+    // };
+
+
 
     $('.btn-next-arr').bind('click',function(){
         $('html,body').animate({scrollTop:'0' + '1072px'},930);
     });
+    // animate가 아닌 ease jquery 로 해야함
+    //스크롤버튼으로 이동한 후에 다른페이지 클릭해도
+   	//새로고침으로 그 페이지의 처음부분이 나오는 것이 아니라
+  	//그 머물렀던 자리에서 시작됨
+
+
+    // $('.amazing-project li a').click(function(){
+    //   var prjid = $(this).attr('id');
+    //     console.log(prjid);
+    //     $('prjid').css('display','block');
+    //     // $('prjid').css('display','block');
+    // })
+    //
+    //   var prjid = $(this).each()
+
+    $('.project-menu li').click(function(){
+    	var idx = $(this).index();
+      console.log(idx);
+    	$('.project-view>ul>li').eq(idx).show().siblings().hide();
+    });
 
     $('.amazing-project li a').click(function(){
-      var prjid = $(this).attr('id');
-        console.log(prjid);
-        $('prjid').css('display','block');
-        // $('prjid').css('display','block');
-    })
+    	var idx = $(this).parent().index();
+      console.log(idx);
+    	$('.project-view>ul>li').eq(idx+1).show().siblings().hide();
+    });
 
-      var prjid = $(this).each()
+
+
+    function popopen() {
+
+         $('.popopen').css({
+           "display":"block"
+         }).append('<iframe width="100%" height="100%" src="https://www.youtube.com/embed/kD3XfMOWPYE?rel=0&autoplay=1&loop=1" frameborder="0" allowfullscreen></iframe>')
+
+         $('.close_btn').click(function(){
+           $('.popopen').css({
+             "display":"none"
+           })
+         })
+    };
+
+
+
 
 
 
 });
-
-//
-// $(document).ready(function(){
-//     $('.btn-next-arr').click(function(){
-//           $('html,body').animate({'scrollTop' : $(this.hash).offset().top},500);
-//           return false;
-//     });
-// };
